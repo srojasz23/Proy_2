e1719af94c34418644a2b33001ca1d684c91e4c7
Stefani Rojas <Stefani Rojas@STEFANI>
2025-11-12 19:21:19 -0500

    -hgg

---
+++ C_Aplicacion/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace C_Aplicacion
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
